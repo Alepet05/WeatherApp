a44aaa72f027c7ad284e8db51f64bfbdd69bf325
Alepet05 <diohunter1337@gmail.com>
2024-05-03 23:31:19 +0300

    Обработка исключений на верхнем уровне

--- main.py
+++ main.py
@@ -3,9 +3,22 @@
 from coordinates import get_gps_coordinates
 from weather import get_weather
 from weather_formatter import format_weather
+from exceptions import CantGetCoordinates, ApiServiceError
 
 
 def main():
-    coordinates = get_gps_coordinates()
-    weather = get_weather(coordinates)
+    try:
+        coordinates = get_gps_coordinates()
+    except CantGetCoordinates:
+        print('Не смогли получить GPS-координаты')
+        exit(1)
+    try:
+        weather = get_weather(coordinates)
+    except ApiServiceError:
+        print('Не смогли получить погоду от API сервиса погоды')
+        exit(1)
     print(format_weather(weather))
+
+
+if __name__ == '__main__':
+    main()
