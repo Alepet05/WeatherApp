6b5ccf9297849042d1bd0e5bea1ea6d35535d89c
Alepet05 <diohunter1337@gmail.com>
2024-05-06 18:48:58 +0300

    Убрал конфиг из .gitignore

---
+++ config.py
@@ -0,0 +1,8 @@
+USE_ROUNDED_COORDS = False
+OPENWEATHER_API_KEY = ''
+OPENWEATHER_URL = (
+    'https://api.openweathermap.org/data/2.5/weather?'
+    'lat={latitude}&lon={longitude}&'
+    'appid=' + OPENWEATHER_API_KEY + '&lang=ru&'
+    'units=metric'
+)
